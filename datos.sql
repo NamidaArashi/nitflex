3f55d3f29258523bdef18e145921a80ed55d9106
Namida <papelytinta00@gmail.com>
2017-05-21 14:49:16 +0200

    Datos sql.

---
+++ datos.sql
@@ -0,0 +1,26 @@
+INSERT INTO pelicula (anyo, director, pais, resumen, titulo, url, img)
+VALUES (1900, 'Tim Burton', 'America', 'blablabla', 'El Viaje de Chihiro', 'https://www.youtube.com/embed/YgO6g_gHtaE','peli1.jpg');
+INSERT INTO pelicula (anyo, director, pais, resumen, titulo, url, img)
+VALUES (1700, 'Rosa', 'España', 'buru', 'Múltiple', 'https://www.youtube.com/embed/L_K9KcxJtVA','peli2.jpg');
+INSERT INTO pelicula (anyo, director, pais, resumen, titulo, url, img)
+VALUES (1800, 'Ana', 'India', 'busdfsu', 'La Bella y la Bestia', 'https://www.youtube.com/embed/4D8N06nx9rs','peli3.jpg');
+INSERT INTO pelicula (anyo, director, pais, resumen, titulo, url, img)
+VALUES (1450, 'Me', 'Londres', 'buru', 'Un Jefe en pañales', 'https://www.youtube.com/embed/ySJjX8Oiuz8','peli4.jpg');
+INSERT INTO pelicula (anyo, director, pais, resumen, titulo, url, img)
+VALUES (1090, 'Óscar', 'Tokyo', 'bara', 'Minions', 'https://www.youtube.com/embed/eU_ANiXZcbI','peli5.jpg');
+INSERT INTO pelicula (anyo, director, pais, resumen, titulo, url, img)
+VALUES (2345, 'Carmen', 'España', 'buru', 'Aliens', 'https://www.youtube.com/embed/u5KPP6lxRVg','peli6.jpg');
+INSERT INTO pelicula (anyo, director, pais, resumen, titulo, url, img)
+VALUES (2003, 'Gustavo', 'Hawaii', 'bere', 'Ghost in the Shell', 'https://www.youtube.com/embed/G4VmJcZR0Yg','peli7.jpg');
+INSERT INTO pelicula (anyo, director, pais, resumen, titulo, url, img)
+VALUES (2010, 'Seenhi', 'Otro', 'buru', 'Las apariencias engañan', 'https://www.youtube.com/embed/QlcNfWtMCRs','peli8.jpg');
+
+INSERT INTO usuario (nombreusr, contrasenya, nombre, apellidos, email, fechanac, pais, ciudad)
+VALUES ('Namida', 'ilikecats', 'Cristina', 'Lopez', 'namida@gmail.com', 1994, 'España', 'Madrid');
+INSERT INTO usuario (nombreusr, contrasenya, nombre, apellidos, email, fechanac, pais, ciudad)
+VALUES ('Mario', '1234', 'Mario', 'Garcia', 'otro@gmail.com', 1987, 'Irlanda', 'Dublin');
+INSERT INTO usuario (nombreusr, contrasenya, nombre, apellidos, email, fechanac, pais, ciudad)
+VALUES ('Haruka', 'muchosushi', 'Haruka', 'Masumizu', 'suchi@gmail.com', 1996, 'Japon', 'Tokyo');
+INSERT INTO usuario (nombreusr, contrasenya, nombre, apellidos, email, fechanac, pais, ciudad)
+VALUES ('John', 'wooo', 'Asier', 'Puerr', 'jap@gmail.com', 1968, 'Francia', 'Paris');
+
